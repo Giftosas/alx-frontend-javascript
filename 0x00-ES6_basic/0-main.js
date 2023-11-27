a9618fc999566f0db0f16a4c8291031eb1d5c265
Gift Osas <osariemenchantelle6@gmail.com>
2023-11-27 22:35:27 +0100

    ES6_basic

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,3 +1,10 @@
+/*
 import { taskFirst, taskNext } from './0-constants.js';
 
 console.log(`${taskFirst()} ${taskNext()}`);
+*/
+
+import taskBlock from './1-block-scoped.js';
+
+console.log(taskBlock(true));
+console.log(taskBlock(false));
