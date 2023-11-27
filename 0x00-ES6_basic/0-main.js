aa561d3d03b79a650e8197cc40e5d4cdcefcc210
Gift Osas <osariemenchantelle6@gmail.com>
2023-11-27 22:39:54 +0100

    ES6_basic

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -3,8 +3,16 @@ import { taskFirst, taskNext } from './0-constants.js';
 
 console.log(`${taskFirst()} ${taskNext()}`);
 */
-
+/*
 import taskBlock from './1-block-scoped.js';
 
 console.log(taskBlock(true));
 console.log(taskBlock(false));
+
+*/
+
+import getNeighborhoodsList from './2-arrow.js';
+
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
